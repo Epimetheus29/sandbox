e4dd44d167331e907f391192035c095866ac15cd
Girish <k99girish@gmail.com>
2026-05-31 16:51:44 +0000

    Added bubble sort functionality

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ async def root():
 async def string_changer(text:str):
     text = text.swapcase()
     return {"message": text}
+
+@app.post("/bubble_sort/{numbers}")
+async def bubble_sort(numbers:str):
+    nums = numbers.split(sep=',')
+    try:
+        parsed_nums = []
+        for i in nums:
+            try:
+                parsed_nums.append(int(i))
+            except ValueError:
+                parsed_nums.append(float(i))
+        nums = parsed_nums
+    except ValueError:
+        return {"message": "Found unsupported types in the Input"}
+    print(nums)
+    n = len(nums)
+    for i in range(0,n):
+        for j in range(0,n-i-1):
+            if (nums[j] > nums[j+1]):
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    
+    return {"message":nums}
